68b41daccd6b144bde8384b69410ffacedf84c9a
Hiroto Nishidate <tak713tak@gmail.com>
2020-06-14 01:34:21 +0000

    commit

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,24 +1,17 @@
-{{-- layouts/profile.blade.phpを読み込む --}}
-@extends('layouts.admin')
+{{-- layouts/profile.blade.phpファイルを読み込む --}}
+@extends('layouts.profile')
+
 
 {{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
 @section('title', 'プロフィールの新規作成')
 
 {{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale1">
-        
-        <title>My Profile</title>
-    </head>
-    <body>
-        <h1>Profile screen</h1>
-        <p> プロフィール 画面</p>
-    </body>
-</html>
-@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
