d141cf2caeb2e2befbbaa89b0cc0c4a621ea75f3
Hiroto Nishidate <tak713tak@gmail.com>
2020-06-28 02:09:26 +0000

    commit

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -31,7 +31,7 @@ rel="stylesheet" type="text/css">
          {{-- Laravel標準で用意されているCSSを読み込みます --}}
          <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
          {{-- この章の後半で作成するCSSを読み込みます　--}}
-         <link href="{{ secure_asset('css/front.css') }}" rel="stylesheet">
+         <link href="{{ asset('css/front.css') }}" rel="stylesheet">
     </head>
     <body>
         <div id="app">
